0095c5dafa3d5b48bad723550f11ad5235c4bb93
duongtt1 <trinhtungduong2019@gmail.com>
2023-03-31 14:44:24 +0700

    Em nhấn chìm tôi xuống bóng hoàng hôn sắp tàn

---
+++ src/rikkei/academy/CrawlSongExample.java
@@ -0,0 +1,38 @@
+package rikkei.academy;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CrawlSongExample {
+    public static void main(String[] args) {
+        try {
+            URL url = new URL("https://www.nhaccuatui.com/bai-hat/nhac-tre-moi.html");
+            // Open the stream and put it into BufferedReader
+            Scanner scanner = new Scanner(new InputStreamReader(url.openStream()));
+            scanner.useDelimiter("\\Z");
+            String content = scanner.next();
+            // Close scanner
+            scanner.close();
+            // Remove all new line
+            content = content.replaceAll("\\n+", "");
+            // Regex
+            Pattern p = Pattern.compile("name_song\">(.*?)</a>");
+            Matcher m = p.matcher(content);
+            while (m.find()){
+                System.out.println(m.group(1));
+            }
+        }catch (MalformedURLException e){
+            e.printStackTrace();
+        }catch (UnsupportedEncodingException e){
+            e.printStackTrace();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
